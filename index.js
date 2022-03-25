2370b8387f3dc8e621fdd5ea3367bf8569baf5f5
shriram <saravananshriram80@gmail.com>
2022-03-26 04:13:17 +0530

    make it better

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const cors = require('cors')
 const path = require('path')
 const axios = require('axios')
 
-const API_KEY = 'AIzaSyDEaW4CaQzmuY66Ca37yszgGom-mbS-v7w'
+const API_KEY = process.env.API_KEY
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // Enable cors
 app.use(cors())
@@ -19,7 +19,6 @@ app.get('/places', async (req, res) => {
   let response = await axios.get(
     `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input_value}&key=${API_KEY}&region=CA&location="46.257114,-63.142780"`
   )
-
   return res.send(
     JSON.stringify(
       response.data.predictions.map((location) => location.description)
